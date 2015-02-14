84bdad452d02548a1f4a5964aa5c40565889990b
David Xue <david.xue.rx@gmail.com>
2015-02-14 11:02:09 -0800

    Added quiz CRUD

---
+++ app/controllers/quizzes_controller.rb
@@ -0,0 +1,54 @@
+class QuizzesController < ApplicationController
+
+  def new
+    @quiz = Quiz.new
+  end
+
+  def create
+    # render text: params
+    @quiz = Quiz.new quiz_params
+    if @quiz.save
+      redirect_to @quiz
+    else
+      render :new
+    end
+  end
+
+  def show
+    @quiz = Quiz.find(params[:id])
+  end
+
+
+  def index
+    @quizzes = Quiz.all
+  end
+
+
+
+  def edit
+    @quiz = Quiz.find(params[:id])
+  end
+
+  def update
+    @quiz = Quiz.find(params[:id])
+    if @quiz.save
+      redirect_to quizzes_path
+    else :edit
+    end
+  end
+
+  def destroy
+    @quiz = Quiz.find (params[:id])
+    @quiz.destroy
+    redirect_to quizzes_path
+
+  end
+
+
+  private
+  def quiz_params 
+    params.require(:quiz).permit(:title, :body, :level, :category)
+  end
+
+
+end
