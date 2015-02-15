da35204e6271444a8bdf31cb41804cc369ded1f4
wxu <winnielandau@gmail.com>
2015-02-14 17:52:54 -0800

    modify fake_quiz_data task to work with User requirements

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20150214215342) do
+ActiveRecord::Schema.define(version: 20150215011155) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "answers", force: :cascade do |t|
+    t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.boolean  "correct"
+  end
 
   create_table "categories", force: :cascade do |t|
     t.string   "title"
