3f809c8a9d27a04d0f9158f8d6ba688fb4953215
Jason <cameljeet@gmail.com>
2015-02-14 21:31:13 -0800

    Styled views for login, signup and edit profile

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150215021716) do
+ActiveRecord::Schema.define(version: 20150215024048) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,8 +36,10 @@ ActiveRecord::Schema.define(version: 20150215021716) do
     t.datetime "updated_at",   null: false
     t.text     "body"
     t.text     "explaination"
+    t.integer  "answer_id"
   end
 
+  add_index "questions", ["answer_id"], name: "index_questions_on_answer_id", using: :btree
   add_index "questions", ["quiz_id"], name: "index_questions_on_quiz_id", using: :btree
 
   create_table "quizzes", force: :cascade do |t|
@@ -66,5 +68,6 @@ ActiveRecord::Schema.define(version: 20150215021716) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
+  add_foreign_key "questions", "answers"
   add_foreign_key "questions", "quizzes"
 end
